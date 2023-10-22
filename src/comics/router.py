1d7bee99f71b58d272768e2de317654d7590b1d1
Maksim Kamanin <ltcatyl@gmail.com>
2023-10-22 10:57:47 +0300

    preview assign fix

--- src/comics/router.py
+++ src/comics/router.py
@@ -4,8 +4,9 @@ from database import firestore
 from typing import Annotated, List
 from .models import Comics
 from sprite.models import Sprite
-from utils import id, blur
+from utils import id
 from constants import Collection, SpriteCategory
+from datetime import datetime
 
 router = APIRouter()
 
@@ -17,14 +18,14 @@ async def get_all() -> List[Comics]:
 @router.post("")
 async def upload(title: Annotated[str, Form()], author: Annotated[str, Form()], file: UploadFile) -> Comics:
     file_id = id()
+    file.filename = title.replace(" ", "-") + '-' + str(file.filename)
     url = await firestore.upload_file(file=file, folder=Collection.Comics, id=file_id)
     all = await firestore.get_all(collection=Collection.Sprite)
     sprites = [Sprite(**one) for one in all if one["category"] == SpriteCategory.Background]
-    preview = blur([sprite.url for sprite in sprites][randint(0, len(sprites) - 1)])
-    preview = UploadFile(file=preview, filename=Collection.Preview) # type: ignore
-    preview_url = await firestore.upload_file(file=preview, folder=Collection.Preview, id=file_id, from_string=True)
-    await firestore.create(collection=Collection.Preview, data={"url": preview_url, "id": file_id})
-    comics = Comics(id=file_id, title=title, author=author, url=url, preview=preview_url) 
+    sprites = list(sorted(sprites, key=lambda sprite: datetime.fromisoformat(str(sprite.created_at)).timestamp()))
+    preview = [sprite.url for sprite in sprites][-1]
+    await firestore.create(collection=Collection.Preview, data={"url": preview, "id": file_id})
+    comics = Comics(id=file_id, title=title, author=author, url=url, preview=preview) 
     sprite = await firestore.create(collection=Collection.Comics, data=comics.model_dump())
     return Comics(**sprite)
 
